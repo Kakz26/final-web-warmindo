a214629752878679309c5eedc060b8e1dfe0a773
Muh Kahlil Kawiwara Faizal <kahlil086@gmail.com>
2024-01-10 11:33:16 +0800

    final web teori

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Email</h3>
+         <a href="mailto:horizonfood@gmail.com">Warmindoketche@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Jam buka</h3>
+         <p>00:07am to 00:05pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Alamat</h3>
+         <a href="#">Bumi kita indonesia</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Nomor WA</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Warmindoketche</span> | all rights reserved!</div>
+
+</footer>
+
+<!-- <div class="loader">
+   <img src="images/loader.gif" alt="">
+</div> -->
